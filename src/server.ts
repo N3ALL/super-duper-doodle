a71a68370879a7c0d6a28f54a168f265daf1ee8d
neal <neal.goossens@student.hogent.be>
2021-10-08 15:11:47 +0200

    refactor

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,10 @@ app.use(express.json());
 
 app.post("/compute", (request, response) => {
   const game = request.body.game;
-  // TODO: Validate input
   if (game == null || game.length != 10){
     response.sendStatus(400);
   } 
-  // TODO: Return response
   else {
-    const score1 = compute(game);
-    response.send({score: score1}).status(200)
+    response.send({score: compute(game)}).status(200)
   }
 });
