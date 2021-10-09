defda54dcb57a52363719cbf3f9165db7d87cfc1
neal <neal.goossens@student.hogent.be>
2021-10-09 16:09:43 +0200

    laatste aanpassing

--- src/compute.ts
+++ src/compute.ts
@@ -3,12 +3,11 @@ import { Game } from "./types";
 export function compute(game: Game): number {
   var TotalScore = 0;
   var FrameArray: any;
-  var isLastFrame = false;
 
   FrameArray = game.flat();
   for (var i = 0; i <= FrameArray.length - 3; i = i + 2){
-    if (isLastFrame == false && i == 18){ //LastFrame
-      isLastFrame = true;
+    if (i == 18){ //LastFrame
+      
       TotalScore = TotalScore + FrameArray[i] + FrameArray[i + 1] + FrameArray[i + 2]
 
     } else if (FrameArray[i] == 10){ // strike
